80024f2561d4b183ccbd5110e4d12e157f72e464
Marko Jovanovic <markus.jovanovic@gmail.com>
2019-08-10 18:16:22 +0200

    Add model and repositories

---
+++ src/main/java/guru/springframework/domain/Ingredient.java
@@ -0,0 +1,54 @@
+package guru.springframework.domain;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.ManyToOne;
+import javax.persistence.OneToOne;
+import java.math.BigDecimal;
+
+@Entity
+public class Ingredient {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name="DESCRIPTION", length=64)
+    private String description;
+
+    @Column(name="AMOUNT")
+    private BigDecimal amount;
+
+    @ManyToOne()
+    private Recipe recipe;
+
+    @OneToOne
+    private UnitOfMeasure unitOfMeasure;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public BigDecimal getAmount() {
+        return amount;
+    }
+
+    public void setAmount(BigDecimal amount) {
+        this.amount = amount;
+    }
+}
